91f6a208ca033ec5b9e112a4845dc5d4ff088a8c
Sukhchain Singh <sukhsingh1566@gmail.com>
2025-05-13 23:18:24 +0530

    Fixed Cloudinary config

--- MyBooksLab/settings.py
+++ MyBooksLab/settings.py
@@ -141,10 +141,9 @@ EMAIL_PORT = 587
 
 EMAIL_HOST_USER = 'sukhsingh15666@gmail.com'
 EMAIL_HOST_PASSWORD = 'vfge hevd sznu fjxm'
-<<<<<<< HEAD
 
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# if not DEBUG:
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import cloudinary
 import cloudinary.uploader
@@ -156,11 +155,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('dtkngdlq4'),
-    'API_KEY': os.getenv('575855677421797'),
-    'API_SECRET': os.getenv('GDLX1XzZXDmegCDpiTrQ1UHytmo'),
+    'CLOUD_NAME': 'dtkngdlq4',
+    'API_KEY': '575855677421797',
+    'API_SECRET': 'GDLX1XzZXDmegCDpiTrQ1UHytmo',
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-=======
->>>>>>> b9ac7d69e6fed5314766425548802200c47f9317
+
